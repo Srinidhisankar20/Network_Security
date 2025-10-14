308bdc1766f691446750a4e5ef3b051f4e24d93f
Srinidhi72 <srinidhis729@gmail.com>
2025-10-14 20:40:45 +0530

    model training added

--- NetworkSecurity/utils/main_utils/utils.py
+++ NetworkSecurity/utils/main_utils/utils.py
@@ -3,6 +3,8 @@ from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
 import os, sys
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pickle 
 
 def read_yaml_file(file_path:str)->dict:
@@ -50,4 +52,46 @@ def save_object(file_path: str, obj: object)->None:
         logging.info("Exited the mainutils , saved the preprocessed object as pickle file")
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
+
+def load_object(file_path:str,) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+
+def load_numpy_array(file_path:str,) -> object:
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            para = param[model_name]   # Correct: use model_name as key
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.components.data_transformation import DataTransformation
+from NetworkSecurity.components.model_trainer import ModelTrainer
 from NetworkSecurity.exception.exception import NetworkSecurityException
-from NetworkSecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from NetworkSecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from NetworkSecurity.logging.logger import logging
 import sys
 
@@ -31,6 +32,12 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        #Model Trainer
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training completed")
         
         
 
